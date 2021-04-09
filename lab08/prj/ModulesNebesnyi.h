edd3ac12d83e87319f47db6e83c65fad2aad5715
unknown <artpro197@gmail.com>
2021-04-09 19:31:51 +0300

    lab09: доповнено проект статичної бібліотеки ModulesNebesnyi.a; до проекту заголовкового файлу додано прототипи функцій.

--- lab08/prj/ModulesNebesnyi.h
+++ lab08/prj/ModulesNebesnyi.h
@@ -2,5 +2,8 @@
 #define MODULESNEBESNYI_H_INCLUDED
 
 float s_calculation(int x, int y, int z);
+float temp_calculation(float *week_tem[], float *Med_F, float *Med_C);
+int rating_actions(int mark);
+int bin_zero_counter(int number);
 
 #endif // MODULESNEBESNYI_H_INCLUDED
--- lab08/prj/ModulesNebesnyi/main.cpp
+++ lab08/prj/ModulesNebesnyi/main.cpp
@@ -1,9 +1,81 @@
 #include <cmath>
 #include <iostream>
-
+#include <wchar.h>
+#include <io.h>
 #include "ModulesNebesnyi.h"
 
 using namespace std;
+
+int bin_zero_counter(int number){
+    int mask = 0b00000000000000000000000000000001;
+    int d12 = 0b00000000000000000000100000000000;
+
+    int BinOne = 0;
+    int BinZero = 0;
+
+    for(int i = 0; i <  sizeof(int) * 8; i++){
+        if(!(number & mask))
+            BinZero++;
+        else
+            BinOne++;
+        mask<<=1;
+    }
+
+    if(number & (mask << 13)){
+        cout << "Binary One - ";
+        return BinOne;
+    }else if(!(number & (mask << 13))){
+        cout << "Binary Zero - ";
+        return BinZero;
+    }
+}
+
+float temp_calculation(float *week_tem[], float *Med_F, float *Med_C){
+    (*Med_F) = 0;
+    (*Med_C) = 0;
+    for(int i = 0; i < 7 ; i++){
+        (*Med_C) = (*Med_C) + (*week_tem[i]);
+    }
+    (*Med_C) = (*Med_C) / 7;
+    (*Med_F) = 32 + (*Med_C) * 9/5;
+}
+
+int rating_actions(int mark){
+    char rating = ' ';
+    if (mark < 0) {
+        return 0.0;
+    } else if (mark > 100) {
+        rating = 'A';
+        wcout << L"Excellent performance with small number of errors - ";
+        return rating;
+    } else if (mark >= 90 && mark <= 100) {
+        rating = 'B';
+        wcout << L"Above average some mistakes - ";
+        return rating;
+    } else if (mark >= 82 && mark <= 89) {
+        rating = 'C';
+        wcout << L"Overall correct fulfillment of certain significant number of errors - ";
+        return rating;
+    } else if (mark >= 75 && mark <= 81) {
+        rating = 'D';
+        wcout << L"Not bad, but with significant number of defects";
+        return rating;
+    } else if (mark >= 67 && mark <= 74) {
+        rating = 'E';
+        wcout << L"satisfies minimum criteria - ";
+        return rating;
+    } else if (mark >= 60 && mark <= 66) {
+        rating = 'F';
+        wcout << L"With a possibility of re-assembly - ";
+        return rating;
+    } else if (mark >= 35 && mark <= 59) {
+        rating = 'X';
+        wcout << L"With the obligatory repeated course - F";
+        return rating;
+    } else {
+        return 0.0;
+    }
+}
 
 float s_calculation(int x, int y, int z){
     float S = pow((2*z + 1), x) - sqrt(abs(y - 1/2 * z)) + 3.14 + z;
